62ec9646fdafb189f099dd5d0b493b7d244d5f6b
Daniele Mariano <danielemariano4@gmail.com>
2022-01-27 13:21:25 +0100

    Solving code smells

--- src/main/java/it/uniroma2/dicii/isw2/jcs/paramTests/App.java
+++ src/main/java/it/uniroma2/dicii/isw2/jcs/paramTests/App.java
@@ -1,13 +1,16 @@
 package it.uniroma2.dicii.isw2.jcs.paramTests;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+	private static final Logger logger = Logger.getLogger(App.class.getName());
+	
+    public static void main( String[] args ) {
+        logger.log( null, "Hello World!" );
     }
 }
